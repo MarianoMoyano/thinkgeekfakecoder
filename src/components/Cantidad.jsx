21c2dedda98da10cb0288e582adde72468e7ca52
Mariano Moyano <marianolmoyano@gmail.com>
2022-07-28 00:17:57 -0300

    funcion carrito

--- src/components/Cantidad.jsx
+++ src/components/Cantidad.jsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import "../components/Cartas/ItemDetail.css"
 import Button from 'react-bootstrap/Button';
 
-export const Cantidad = ({id}) => {
+export const Cantidad = ({id, nombre}) => {
     const [dataCantidad, setDataCantidad] = useState(0)
     return(
-       <div>
+       <div key={id}>
         <Button onClick={()=> {
             if (dataCantidad < id)
             setDataCantidad ( dataCantidad+1)
             }}>+</Button>
-        Cantidad: {dataCantidad}  
+        <span className="grandeNumero carrito">{dataCantidad}</span>
         <Button onClick={()=> dataCantidad && setDataCantidad (dataCantidad-1)}>-</Button>
-       <h3>Cantidad disponible: {id}</h3>
+        <Button className="carrito" onClick={() => sumarAlCarro (nombre, id, dataCantidad)}>Sumar al carro</Button>
+        <h3>Cantidad disponible: {id}</h3>
        </div> 
     )
+}
+
+function sumarAlCarro (nombre, id, Cantidad){
+    localStorage.setItem (id, JSON.stringify ({nombre:nombre, Cantidad:Cantidad, id:id}))
 }
 
 export default Cantidad

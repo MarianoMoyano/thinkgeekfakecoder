2f6acb5e477fc1db5763d060edc87ca227e2af21
Mariano Moyano <marianolmoyano@gmail.com>
2022-08-22 00:42:21 -0300

    brujo api parte 3

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -3,48 +3,55 @@ import { useParams } from "react-router-dom";
 import {getDoc, getDocs, doc, docs, getFirestore, query, limit, where, collection} from "firebase/firestore";
 import { BrujoDetalle } from "./Cartas";
 import "../components/Cartas/ItemDetail.css";
+import Cargando from "./Cargando";
 
 const Categoria = () => {
   const { id } = useParams();
-    const [dataCategoria, setDataCategoria] = useState([]);
-    const [dataCarrito, setDataCarrito] = useState([])
+  const [dataCategoria, setDataCategoria] = useState([]);
+  const [dataCarrito, setDataCarrito] = useState([])
+  const [cargando, setCargando] = useState (true)
 
-    useEffect(() => {
-      const db = getFirestore();
-      const productos = collection (db, "productos");
-      const filteredCollection = query(
-        productos,
-        where("idCategoria", "==", parseInt(id))
-      );
-      getDocs (filteredCollection).then (snapshot=> {
-        const datos= snapshot.docs.map (doc=> ({
-          id: doc.id,
-          ...doc.data()
-        })) 
-        console.log(datos)
-        setDataCategoria(datos)
+  useEffect(() => {
+    setCargando (true)
+    const db = getFirestore();
+    const productos = collection (db, "productos");
+    const filteredCollection = query(
+      productos,
+      where("idCategoria", "==", parseInt(id))
+    );
+    getDocs (filteredCollection).then (snapshot=> {
+      const datos= snapshot.docs.map (doc=> ({
+        id: doc.id,
+        ...doc.data()
+      })) 
+      console.log(datos)
+      setDataCategoria(datos)
 
-        setDataCarrito ( JSON.parse (localStorage.getItem ("carrito") || "[]"))
-      })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-  
+      setDataCarrito ( JSON.parse (localStorage.getItem ("carrito") || "[]"))
+    }) 
+    setCargando (false)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-      <div className="grid-Cartas grid-fondo"> 
-          { dataCategoria.map((brujo) => (
-            <div>
-                  <BrujoDetalle
-                    id={brujo.id}
-                    nombre={brujo.nombre} 
-                    precio={brujo.precio} 
-                    imagen={brujo.imagen}
-                    cantidadInicial={dataCarrito.filter((item)=> {return item.id===brujo.id}).Cantidad}
-                    stock={brujo.stock}/>
-                  </div>))
-          }
-      </div> 
-      );
-  };
-;
+  if (cargando) {
+    return <Cargando/>;
+  }
+
+  return (
+    <div className="grid-Cartas grid-fondo"> 
+        { dataCategoria.map((brujo) => (
+          <div>
+                <BrujoDetalle
+                  id={brujo.id}
+                  nombre={brujo.nombre} 
+                  precio={brujo.precio} 
+                  imagen={brujo.imagen}
+                  cantidadInicial={dataCarrito.filter((item)=> {return item.id===brujo.id}).Cantidad}
+                  stock={brujo.stock}/>
+                </div>))
+        }
+    </div> 
+    );
+};
+
 export default Categoria;
